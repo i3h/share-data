367a3c8fcfaa21b85e7a6ab5115ca385dfbaa7c5
noobly314 <noobly314@gmail.com>
2020-04-07 12:34:56 +0800

    release v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# share-variable
+# share-data
+
+This GitHub Action shares data between Jobs with help of artifact.
+
+```
+- name: Set data
+  id: set_data
+  uses: noobly314/share-data@v1
+  with:
+    share-id: 'testing'
+    mode: set
+    key: username
+    value: myname
+```
--- src/share-variable.js
+++ src/share-variable.js
@@ -1,19 +1,65 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const artifact = require('@actions/artifact');
+const fs = require('fs').promises;
+
+// Setup user input
+const share_id = core.getInput('share-id');
+const mode = core.getInput('mode');
+const key = core.getInput('key');
+const value = core.getInput('value');
+
+// Setup artifact
+const artifactClient = artifact.create();
+const artifactName = share_id;
+const fileName = `share_data_${share_id}.json`;
+const rootDirectory = '.';
+const files = [fileName];
 
 async function run() {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(`The event payload: ${payload}`);
+    let js = {};
+    if (mode == 'set') {
+      js = await load();
+      js[key] = value;
+      await save(js);
+    } else if (mode == 'get') {
+      js = await load();
+      let data;
+      if (key != '') {
+        data = js[key];
+      } else {
+        data = JSON.stringify(js);
+      }
+      core.setOutput('data', data);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
+}
+
+async function load() {
+  let js = {};
+  try {
+    let downloadResponse = await artifactClient.downloadArtifact(artifactName);
+    let raw = await fs.readFile(fileName);
+    js = JSON.parse(raw);
+    await fs.unlink(fileName);
+  } catch (error) {
+    //console.log(error);
+  }
+  return js;
+}
+
+async function save(js) {
+  let data = JSON.stringify(js, null, 2);
+  await fs.writeFile(fileName, data, { mode: '644' });
+  let uploadResult = await artifactClient.uploadArtifact(
+    artifactName,
+    files,
+    rootDirectory,
+  );
+  await fs.unlink(fileName);
 }
 
 module.exports = run;
